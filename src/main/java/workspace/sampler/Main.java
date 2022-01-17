b7e15e6a01e3902ca17343ddac73b273324e090d
Noa <noa27102@gmail.com>
2022-01-17 10:58:28 +0200

    Added a main file to check if the code is working

--- src/main/java/workspace/sampler/Main.java
+++ src/main/java/workspace/sampler/Main.java
@@ -1,14 +1,23 @@
 package workspace.sampler;
 
 
-import java.io.*;
-import java.util.HashSet;
-import java.util.List;
+import workspace.sampler.Objects.SampledPerson;
+import workspace.sampler.fileExtract.CsvReader;
+import workspace.sampler.fileLoader.JsonFileLoader;
+
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         String fileName = "src/main/resources/MadaReports.csv";
+        CsvReader csvReader = new CsvReader();
+        JsonFileLoader jsonFileLoader = new JsonFileLoader();
 
-
+        Set<SampledPerson> sampledPersonSet = csvReader.fileCsvReader(fileName);
+        jsonFileLoader.jsonToCsvSerialization(sampledPersonSet);
+        //jsonCsvSerializer.jsonToCsvSerialization(sampledPersonSet);
+        /*for (SampledPerson person:sampledPersonSet) {
+            System.out.println(person);
+        }*/
     }
 }
