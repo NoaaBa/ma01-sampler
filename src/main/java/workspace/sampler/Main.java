a9a5fc31a9f485375e4bfcaf6be367b6011cc634
Noa <noa27102@gmail.com>
2022-01-17 11:59:30 +0200

    Updated main to use the JsonManger

--- src/main/java/workspace/sampler/Main.java
+++ src/main/java/workspace/sampler/Main.java
@@ -1,23 +1,10 @@
 package workspace.sampler;
 
-
-import workspace.sampler.Objects.SampledPerson;
-import workspace.sampler.fileExtract.CsvReader;
-import workspace.sampler.fileLoader.JsonFileLoader;
-
-import java.util.Set;
+import workspace.sampler.Managers.JsonManager;
 
 public class Main {
-    public static void main(String[] args) {
-        String fileName = "src/main/resources/MadaReports.csv";
-        CsvReader csvReader = new CsvReader();
-        JsonFileLoader jsonFileLoader = new JsonFileLoader();
 
-        Set<SampledPerson> sampledPersonSet = csvReader.fileCsvReader(fileName);
-        jsonFileLoader.jsonToCsvSerialization(sampledPersonSet);
-        //jsonCsvSerializer.jsonToCsvSerialization(sampledPersonSet);
-        /*for (SampledPerson person:sampledPersonSet) {
-            System.out.println(person);
-        }*/
+    public static void main(String[] args) {
+        JsonManager.csvToJsonHandler();
     }
 }
