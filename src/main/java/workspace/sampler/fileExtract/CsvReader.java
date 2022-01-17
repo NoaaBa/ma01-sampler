5a4bcdf2eac918b2f564c1b439d937092d0139e4
Noa <noa27102@gmail.com>
2022-01-17 10:57:45 +0200

    Updated file -> removed toDate() function because there is no need for it

--- src/main/java/workspace/sampler/fileExtract/CsvReader.java
+++ src/main/java/workspace/sampler/fileExtract/CsvReader.java
@@ -3,21 +3,22 @@ package workspace.sampler.fileExtract;
 import workspace.sampler.Objects.SampledPerson;
 
 import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Set;
 
 public class CsvReader {
 
+    public CsvReader() {}
+
     public Set<SampledPerson> fileCsvReader(String fileName) {
-        Set<SampledPerson> sampledPeople = new HashSet<SampledPerson>();
+        Set<SampledPerson> sampledPeople = new HashSet<>();
         Path pathToFile = Paths.get(fileName);
-        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.US_ASCII)) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line = br.readLine();
+
             while (line != null) {
                 String[] attributes = line.split(",");
                 SampledPerson sampledPerson = createPerson(attributes);
@@ -32,11 +33,6 @@ public class CsvReader {
 
     private SampledPerson createPerson(String[] record) {
         return new SampledPerson(record[0], record[1], record[2], record[3],
-                record[4], record[5], record[6],record[7],record[8],toDate(record[9]), toDate(record[10]), toDate(record[11]));
-    }
-
-    public LocalDate toDate(String date) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return LocalDate.parse(date, formatter);
+                record[4], record[5], record[6],record[7],record[8],record[9], record[10], record[11]);
     }
 }
