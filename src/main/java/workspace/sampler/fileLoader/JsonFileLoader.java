ac6c3294a352b8d2e9658c3ec5cb2729ef752ff4
Noa <noa27102@gmail.com>
2022-01-17 10:53:54 +0200

    Realized that there is no need to use a serialization and that there are better libraries to use in order to parse the object to file of jsons

--- src/main/java/workspace/sampler/fileLoader/JsonFileLoader.java
+++ src/main/java/workspace/sampler/fileLoader/JsonFileLoader.java
@@ -1,4 +1,42 @@
 package workspace.sampler.fileLoader;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import workspace.sampler.Objects.SampledPerson;
+
+import java.io.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
 public class JsonFileLoader {
+
+    final static int maxJsonInFile = 200;
+
+    public void jsonToCsvSerialization(Set<SampledPerson> sampledPersonSet) {
+        Set<SampledPerson> smallerJsonSet = new HashSet<>();
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        int counter = 0, fileNum = 0;
+        try {
+            for (SampledPerson person:sampledPersonSet){
+                smallerJsonSet.add(person);
+                counter++;
+                if(counter == maxJsonInFile) {
+                    gson.toJson(smallerJsonSet, new FileWriter("src/main/madaSampler/mada" + fileNum + ".json"));
+                    fileNum++;
+                    smallerJsonSet.clear();
+                    counter = 0;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error processing CSV to json.");
+        }
+    }
+
+    public static void loadJsonSetToFile() {
+
+    }
 }
