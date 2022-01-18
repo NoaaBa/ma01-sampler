734ad6eda5d8b118ae1f1c145eed2be8d8d97559
Noa <noa27102@gmail.com>
2022-01-18 11:05:29 +0200

    Contains file manager which calls the functions that the json and xml use

---
+++ src/main/java/workspace/sampler/Managers/FilesManager.java
@@ -0,0 +1,22 @@
+package workspace.sampler.Managers;
+
+import workspace.sampler.fileExtract.CsvReader;
+import workspace.sampler.fileLoader.JsonFileLoader;
+import workspace.sampler.fileTransformer.ObjectToXmlConverter;
+
+public class FilesManager {
+
+    public FilesManager() {}
+
+    protected CsvReader getCsvReader() {
+        return new CsvReader();
+    }
+
+    protected JsonFileLoader getJsonFileLoader() {
+        return new JsonFileLoader();
+    }
+
+    protected ObjectToXmlConverter getObjectToXmlConverter() {
+        return new ObjectToXmlConverter();
+    }
+}
